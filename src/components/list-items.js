609cb9de389ed3bde26ea462a30348e866d685d0
Patricio Vargas <pevargas2730@gmail.com>
2019-09-07 04:33:01 -0400

    styles

--- src/components/list-items.js
+++ src/components/list-items.js
@@ -22,14 +22,54 @@ export class ListItems extends LitElement {
   render() {
     this.todoList = JSON.parse(this.todoList);
     return html`
-      <ul>
-        ${this.todoList.map(
-          todo =>
-            html`
-              <todo-item todoItem=${JSON.stringify(todo)}></todo-item>
-            `
-        )}
-      </ul>
+      <style>
+        .lists {
+          padding-left: 350px;
+          margin: 0 auto;
+          max-width: 500px;
+        }
+        .list {
+          transform-origin: center bottom;
+          transition: 200ms all linear;
+        }
+        .list .title {
+          color: #b8d4ff;
+          font-size: 1.5rem;
+          letter-spacing: 5px;
+          text-transform: uppercase;
+          text-align: center;
+          margin: 3.5rem 0 3.5rem 0;
+          line-height: 1;
+        }
+        .list .list-wrapper {
+          list-style: none;
+          margin: 0 0.5rem;
+          padding: 0;
+        }
+        @media (max-width: 576px) and (orientation: portrait) {
+          .lists {
+            padding: 0 1rem;
+            margin-bottom: 5rem;
+          }
+          .list .title {
+            margin: 1.5rem 1rem;
+            font-size: 1.5rem;
+          }
+        }
+      </style>
+      <div class="lists">
+        <div class="list>
+        <h2 class="title">Today's To Do List</h2>
+        <div class="list-wrapper">
+          ${this.todoList.map(
+            todo =>
+              html`
+                <todo-item todoItem=${JSON.stringify(todo)}></todo-item>
+              `
+          )}
+        </div>
+      </div>
+      <div></div>
     `;
   }
 }
