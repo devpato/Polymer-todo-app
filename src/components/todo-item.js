b94adb63578c9902e7f0bcfe201151f80f70a3ef
Patricio Vargas <pevargas2730@gmail.com>
2019-09-06 22:03:43 -0400

    on remove despatchin event

--- src/components/todo-item.js
+++ src/components/todo-item.js
@@ -15,10 +15,21 @@ export class TodoItem extends LitElement {
     super();
   }
 
+  onRemove() {
+    this.dispatchEvent(
+      new CustomEvent('removeItem', {
+        bubbles: true,
+        composed: true,
+        detail: { itemId: this.todoItem.id }
+      })
+    );
+  }
+
   render() {
     this.todoItem = JSON.parse(this.todoItem);
     return html`
       <li>${this.todoItem.item}</li>
+      <button @click="${this.onRemove}">Remove</button>
     `;
   }
 }
